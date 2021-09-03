0c0f00a53a66c52a2da2401bed4dcc29f4bfc475
Xerbeth <faiber.torres.o@gmail.com>
2021-09-03 09:22:31 -0500

    Depración de SP
    Se agrega validación de rango de fechas, se eliminan variables y se ajusta mensajes

--- DataBase/3. sp.sql
+++ DataBase/3. sp.sql
@@ -8,7 +8,6 @@ BEGIN
 	BEGIN
 		DECLARE @Existence INT
 		DECLARE @OpeningTime TIME
-		DECLARE @CloseTime TIME
 		DECLARE @Duration INT
 		DECLARE @OpenTimeAux TIME
 		DECLARE @CloseTimeAux TIME
@@ -17,15 +16,22 @@ BEGIN
 		DECLARE @DaysAux INT = 0
 		DECLARE @Shift_ INT = 0 -- Número de turnos por día
 		DECLARE @ShiftAux INT = 0
-		DECLARE @ServiceId_ INT = @ServiceId
+		DECLARE @ShiftsGenerated INT = 0
 	END -- Fin bloque declaración de variables SP
 	-- BLOQUE PRINCIPAL
-	BEGIN		
+	BEGIN
+		BEGIN -- Bloque de validación de rango de fechas
+			IF @EndDate < @StartDate
+			BEGIN 
+				SET @Message = 'La fecha final no puede ser menor a la fecha inicial'
+				RETURN @Message
+			END
+		END -- Fin bloque de validación de rango de fechas
 		BEGIN -- Bloque para validar la existencia del identificador del servicio
 			BEGIN TRY -- Control de ejecución de bloque 
 				SELECT @Existence = CAST(COUNT(1)  AS INT)
 				FROM develop.Services 
-				WHERE Service_Id = @ServiceId_
+				WHERE Service_Id = @ServiceId
 				IF @Existence = 0
 				BEGIN 
 					SET @Message = 'No existe el identificador del servicio'
@@ -41,7 +47,6 @@ BEGIN
 			BEGIN TRY -- Control de ejecución de bloque 
 				SELECT @OpeningTime = Opening_Time,
 					   @OpenTimeAux = Opening_Time,
-					   @CloseTime = Close_Time,					   
 					   @Duration = Duration,
 					   @CloseTimeAux = Close_Time
 				FROM develop.Services  
@@ -54,28 +59,25 @@ BEGIN
 		END -- Fin bloque para consultar la hora de apertura, la hora de cierre y la duración del servicio
 		BEGIN			
 			BEGIN TRY -- Control de ejecución de bloque
-				SET @Days = (SELECT DATEDIFF(DAY, @StartDate, @EndDate)) -- Cantidad de ciclos por dia
+				SET @Days = (SELECT DATEDIFF(DAY, @StartDate, @EndDate)) + 1 -- Cantidad de ciclos por dia
 				SET @Shift_ = (SELECT DATEDIFF(MINUTE, Opening_Time, Close_Time)/Duration FROM develop.Services WHERE Service_Id = @ServiceId) -- Diferencia en minutos de la hora de inico y la hora de cierre y se divide en la duración del turno para obtener la cantidad de turnos por día
-				IF @Days = 0 -- Si la diferencia es de cero días				
-				BEGIN 
-					SET @Days = 1 --se inicializa en 1 para un generación del día actual
-				END 
 				WHILE (@DaysAux < @Days)
 				BEGIN -- Inicio while dias	
 					SET @ShiftAux = 0
+					SET @OpenTimeAux = @OpeningTime
 					WHILE (@ShiftAux < @Shift_)
 					BEGIN	
 						SET @CloseTimeAux = (SELECT DATEADD(MINUTE,@Duration,@OpenTimeAux) FROM develop.Services)
-						--SELECT @ServiceId_, @StartDateAux, @OpenTimeAux, @OpenTimeAux
 						INSERT INTO develop.Shifts (service_id, Shift_Date, Start_Time, End_Time)
-				        VALUES (@ServiceId_,@StartDateAux,@OpenTimeAux,@CloseTimeAux)
+				        VALUES (@ServiceId,@StartDateAux,@OpenTimeAux,@CloseTimeAux)
 						SET @OpenTimeAux = (SELECT DATEADD(MINUTE,@Duration,@OpenTimeAux) FROM develop.Services)
 						SET @ShiftAux = @ShiftAux + 1
+						SET @ShiftsGenerated = @ShiftsGenerated + 1
 					END
 					SET @StartDateAux = (SELECT DATEADD(DAY,1,@StartDateAux)) -- Se aumenta la sigiente fecha de generación de turnos
 					SET @DaysAux = @DaysAux + 1 -- se aumenta la variable de ciclos por día
 				END -- Fin while dias			
-				SET @Message = 'Solicitud de reserva realizada satisfactoriamente'
+				SET @Message = 'Solicitud de reserva realizada satisfactoriamente. Total de turnos generados ' + CAST(@ShiftsGenerated as varchar(1000))
 			END TRY  
 			BEGIN CATCH
 				SET @Message = 'Ocurrió un error registrando la reserva'
